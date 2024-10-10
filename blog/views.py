c188a46977c91aaf9806c52d01f3b0208e4c6158
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-10-10 18:40:11 +0300

    swapped hard coded part with dynamic ones

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.http import HttpResponse
 from django.shortcuts import render
 
-posts =[
+all_posts =[
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.png",
@@ -73,8 +73,16 @@ posts =[
 ]
 
 
+def get_date(post):
+    return post['date']
+
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_post = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_post
+    })
 
 
 def posts(request):
